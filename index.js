b6928c00f6dd6ec42c9d0163a6bc23b6a37efcdf
elegant-architecture-3891 <katherineortiz313@gmail.com>
2019-03-18 15:26:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var clone = Object.assign({}, object);
   delete clone[key];
-  return clone 
+  return clone
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  return Object.assign({}, object, {[key]: value});
+  delete object[key];
+  return object 
 }
