24a781eac7e10eb94eea12b22b557d1127c69692
elegant-architecture-3891 <katherineortiz313@gmail.com>
2019-03-18 15:26:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object);
+  delete clone[key];
+  return clone 
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  return Object.assign({}, object, {[key]: value});
+}
